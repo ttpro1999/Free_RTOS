0fea7744bd297e2019b943c41fc0cee6b6187c6e
ttpro1999 <50543333+ttpro1999@users.noreply.github.com>
2020-08-13 14:53:45 +0700

    Create BT1.c

---
+++ BT1.c
@@ -0,0 +1,52 @@
+#include <Arduino_FreeRTOS.h>
+
+// Các lệnh I/O của arduino
+// digitalRead, digitalWrite,Serial.Print,pinMode
+
+void Task_Controlled(void *param);
+void Task_print(void *param);
+
+
+
+void setup()
+{
+  Serial.begin(9600);
+  pinmode(8,INPUT_PULLUP);
+  pinmode(13,OUTPUT);
+}
+
+
+void loop()
+{
+  // DO nothing
+}
+
+void Task_Controlled(void *param)
+{
+  while (1)
+  {
+    if (digitalRead(8) == 0)
+    {
+      while (digitalRead(8)==0)
+      {
+        digitalWrite((13),LOW);
+      }
+    
+    }
+    else digitalWrite((13),HIGH);
+    vTaskDelay(1000/portTICK_PERIOD_MS);//Délay 1s
+  }
+  
+  void Task_print(void *param)
+  {
+    void *param;
+    while (1)
+    {
+      Serial.print('Task_print')
+      vTaskDelay(1000/portTICK_PERIOD_MS);//Delay 1s
+    }
+    
+  }
+    
+}
+
